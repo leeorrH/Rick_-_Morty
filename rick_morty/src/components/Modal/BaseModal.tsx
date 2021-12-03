bf26ecbcc26e2bd0e2eedd03dcdbef19a9e3f311
leeorrH <leeorrh@gmail.com>
2021-12-03 15:28:38 +0200

    need to finish modal

---
+++ rick_morty/src/components/Modal/BaseModal.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+interface ModalProps {
+  openModal : boolean
+}
+
+export const BaseModal: React.FC<ModalProps> = ({children, openModal}) => {
+  const [open, setOpen] = React.useState(openModal);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+         {children}
+        </Box>
+      </Modal>
+    </div>
+  );
+}
